67f735130dc03c8bc5c7e96855756568c2b6a77f
KrispyWaffles <173952614+KrispyWaffles@users.noreply.github.com>
2024-09-19 20:26:11 -0700

    add logic for user choice with Invalid prompt for wrong selection

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 function getComputerChoice (arr) {
-    const randomSelection = Math.floor(Math.random() * arr) 
+    const randomSelection = Math.floor(Math.random() * arr.length); 
     return arr[randomSelection];
 }
 
 const picks = ["rock", "paper", "scissors" ];
 const randomChoice = getComputerChoice(picks);
 
-console.log(getComputerChoice);
 
+console.log(randomChoice);
+
+function getMyChoice () {
+    let choice = prompt("Make a Choice. Rock, Paper or Scissors");
+
+    if (choice !== "rock" || "paper" || "scissors") {
+        return prompt("Invalid Selection");
+    }
+
+
+}
+getMyChoice();
