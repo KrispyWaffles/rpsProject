742dc113d24cc319cd7bf2cd4f99ceb755578150
KrispyWaffles <173952614+KrispyWaffles@users.noreply.github.com>
2024-09-19 21:46:26 -0700

    trying to figure out getMyChoice logic .Trying to compare it to randomChoice picks

--- script.js
+++ script.js
@@ -3,19 +3,41 @@ function getComputerChoice (arr) {
     return arr[randomSelection];
 }
 
-const picks = ["rock", "paper", "scissors" ];
+const picks = ["rock", "paper", "scissors"];
 const randomChoice = getComputerChoice(picks);
 
 
 console.log(randomChoice);
 
-function getMyChoice () {
-    let choice = prompt("Make a Choice. Rock, Paper or Scissors");
+// function getMyChoice () {
+//     let choice = prompt("Make a Choice. Rock, Paper or Scissors");
+//     console.log(choice)
+//     if (choice !== "rock" || choice !== "paper" || choice !== "scissors") {
+//         console.log(choice)
+//         return prompt("Invalid Selection");
+//     }
+//     console.log(choice);
 
-    if (choice !== "rock" || "paper" || "scissors") {
-        return prompt("Invalid Selection");
+// }
+// getMyChoice();
+
+
+function getMyChoice () {
+    let selection = ["rock", "paper", "scissors"];
+    let choice = prompt("Make a Choice. Rock, Paper or Scissors");
+   
+    if (choice !== selection) {
+        return prompt("Invaild Selection. Try Again!");
+    }
+        
+    if (choice === picks) {
+        return ("Your Correct");
+    } 
+    
+    if (choice !== picks)
+        return ("Wrong Answer!");
     }
 
+    // console.log(choice);
 
-}
 getMyChoice();
